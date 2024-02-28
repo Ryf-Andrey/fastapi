a3b0a592c6c25748be54332a7e0ecad3f09fd334
Ryf-Andrey <phpadvancephp@gmail.com>
2024-02-28 09:29:01 +0200

    sec

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
 
+from auth.auth import auth_backend
 
-app = FastAPI()
+from fastapi_users import FastAPIUsers
 
-@app.get("/")
-def hello():
-    return "Hello"
+from auth.schemas import UserCreate, UserRead
+
+from auth.manager import get_user_manager
+from auth.database import User
+app = FastAPI(
+    title="Trading app"
+)
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
